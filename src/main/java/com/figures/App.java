5b9308c6c0b3691bdc363254e13c1a5b5971398a
Artemiy Viazovski <grenkavoit@gmail.com>
2024-10-18 14:50:12 +0300

    project created 14:50 18.10.2024

---
+++ src/main/java/com/figures/App.java
@@ -0,0 +1,13 @@
+package com.figures;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
